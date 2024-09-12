603bbd4243022d22bc00edef729fc3188c7ffa79
wardennkoil <109043302+wardennkoil@users.noreply.github.com>
2024-09-12 11:56:26 -0400

    week 2 lab

---
+++ microservices-parent/src/test/java/ca/gbc/microservicesparent/MicroservicesParentApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.gbc.microservicesparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
